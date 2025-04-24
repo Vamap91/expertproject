23e37e2f0aa8c5dd77ab5ad11133fca2f7b1f7f7
Vamap91 <viniciusaugustopaschoa1@gmail.com>
2025-04-24 13:31:52 -0300

    Update audio_generator.py

--- touch utils/audio_generator.py
+++ touch utils/audio_generator.py
@@ -1,1 +1,13 @@
+# utils/audio_generator.py
+
+import pyttsx3
+import tempfile
+
+def text_to_audio(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 160)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
+    engine.save_to_file(text, temp_file.name)
+    engine.runAndWait()
+    return open(temp_file.name, "rb").read()
 
